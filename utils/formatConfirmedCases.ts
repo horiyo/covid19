8e79c400419efd5f11a051fd83e27d3b05750914
Yoshiaki Hori <hori@hori-s.net>
2020-05-08 19:52:53 +0900

    Update formatConfirmedCases.ts

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -50,8 +50,8 @@ export default (data: DataType) => {
   const formattedData: ConfirmedCasesType = {
     検査実施人数: data.value,
     陽性物数: data.children[0].value,
-    宿泊施設療養: data.children[0].children[0].value,
-    ホテル宿泊: data.children[0].children[1].value,
+    入院中: data.children[0].children[0].value,
+    宿泊施設療養: data.children[0].children[1].value,
     退院: data.children[0].children[2].value,
     死亡: data.children[0].children[3].value,
     県外: data.children[0].children[4].value
